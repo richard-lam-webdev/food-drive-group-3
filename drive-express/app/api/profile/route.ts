bb2ae28c49de30c50e91573b18438bacf07219b5
Dams25 <damien2504@outlook.fr>
2025-02-13 00:57:09 +0100

    Petite correction error

--- drive-express/app/api/profile/route.ts
+++ drive-express/app/api/profile/route.ts
@@ -2,8 +2,8 @@
 import prisma from '@/lib/prisma';
 import { NextResponse } from 'next/server';
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route"; 
-export async function GET(req: Request) {
+import { authOptions } from "@/lib/authOptions";
+export async function GET() {
   
   const session = await getServerSession(authOptions);
   
